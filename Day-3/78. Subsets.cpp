ff598eef26db2f651ff67961981f7390d13f42b9
Swati Kumari <swati.30.kumari.05@gmail.com>
2026-08-23 17:51:43 +0530

    Update 78. Subsets.cpp

--- Day-3/78. Subsets.cpp
+++ Day-3/78. Subsets.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> r;
 
+    void f(vector<int>& a,int i,vector<int>& v) {
+        if(i==a.size()) {
+            r.push_back(v);
+            return;
+        }
+        f(a,i+1,v);
+        v.push_back(a[i]);
+        f(a,i+1,v);
+        v.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& a) {
+        vector<int> v;
+        f(a,0,v);
+        return r;
+    }
+};

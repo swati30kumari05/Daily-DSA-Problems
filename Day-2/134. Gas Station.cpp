922aa4ef06553a109d95993ce9ec5e447bdcd1fa
Swati Kumari <swati.30.kumari.05@gmail.com>
2026-08-23 17:46:54 +0530

    Create 134. Gas Station.cpp

---
+++ Day-2/134. Gas Station.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total=0,cur=0,start=0;
+        for(int i=0;i<gas.size();i++) {
+            total+=gas[i]-cost[i];
+            cur+=gas[i]-cost[i];
+            if(cur<0) cur=0,start=i+1;
+        }
+        return total<0 ? -1 : start;
+    }
+};

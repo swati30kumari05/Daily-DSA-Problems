0044c15667b55baf6b88ec7e5c3ef79d3338f851
Swati Kumari <swati.30.kumari.05@gmail.com>
2026-08-24 09:34:51 +0530

    Create 724. Find Pivot Index.cpp

---
+++ Day-6/724. Find Pivot Index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0), left = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (left == total - left - nums[i])
+                return i;
+            left += nums[i];
+        }
+        
+        return -1;
+    }
+};
